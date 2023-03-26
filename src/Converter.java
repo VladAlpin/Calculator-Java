93c7087284a197302c518c971e19c7f180671897
VladAlpin <zxczxczxc33513@gmail.com>
2023-03-26 23:19:36 +1000

    Calculator

---
+++ src/Converter.java
@@ -0,0 +1,66 @@
+import java.util.TreeMap;
+
+class Converter {
+    // римские числа
+    TreeMap<Character, Integer> romanKey = new TreeMap<>();
+
+    // арабские числа
+    TreeMap<Integer, String> arabianKey = new TreeMap<>();
+
+    // массив содержащий римские цифры и ключ к ним
+    public Converter() {
+        romanKey.put('I', 1);
+        romanKey.put('V', 5);
+        romanKey.put('X', 10);
+
+        arabianKey.put(10, "X");
+        arabianKey.put(9, "IX");
+        arabianKey.put(5, "V");
+        arabianKey.put(4, "IV");
+        arabianKey.put(1, "I");
+    }
+
+    // проверяет ввыденную строчку, и конвертирует с помощью chatAt в 'V' из "V", с помощью containsKey проверяет его наличие под данным индексом
+    // берет ключ, и с ним проводит логические операции, если написать '2', то он ее не найдет это число т.к. его нет в массиве
+    boolean isRoman(String number){
+        return romanKey.containsKey(number.charAt(0));
+    }
+
+    //римские в арабские, если IV
+    int romanToInt(String s) {
+        int end = s.length() - 1;
+        // создает массив, и метод toCharArray когда получает число, делает из него массив, {'I', 'V'}
+        char[] arr = s.toCharArray();
+        int arabian;
+        // добавляет все в результат
+        int result = romanKey.get(arr[end]);
+        for (int i = end - 1; i >= 0; i--) {
+            arabian = romanKey.get(arr[i]);
+            // ищит подходящее число, конвертируем в арабское, и прибовляет его к результату
+            if (arabian < romanKey.get(arr[i + 1])) {
+                result -= arabian;
+            } else {
+                result += arabian;
+            }
+        }
+        return result;
+    }
+
+    //арабские в римские, если получается 6
+    String intToRoman(int number) {
+        String roman = "";
+        int arabianKey;
+        do {
+            // floorKey ищет ключ из массива начиная с низу
+            arabianKey = this.arabianKey.floorKey(number);
+            // конвертирует в римское число и прибовляет к roman
+            roman += this.arabianKey.get(arabianKey);
+            // отнимает 5 от начального числа, так как прижайшее число по массиву было 5(V), и остается 1
+            number -= arabianKey;
+            // так как осталось еще 1, массив заного запускается
+        } while (number != 0);
+        // возвращает VI
+        return roman;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // запускает программу и считавает введеные значения в консоль
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(calc(input));
+    }
+
+    public static String calc(String input) {
+        Converter converter = new Converter();
+
+        // массив занков
+        String[] actions = {"+", "-", "/", "*"};
+        // просто так плюс или умножить не передать, перед ним нужно поставить два слеша
+        String[] regexActions = {" \\+ ", " - ", " / ", " \\* "};
+
+        // перечесляет знаки
+        int actionIndex = 0;
+        for (int i = 0; i < actions.length; i++) {
+            if (input.contains(actions[i])) {
+                actionIndex = i;
+                break;
+            }
+        }
+        //создает массив всех чисел и разбивает строку на части с помощью split, знаки +-*/ кладет в массив
+        String[] data = input.split(regexActions[actionIndex]);
+        // ошибки
+        if(data.length < 2)  try {
+            System.out.println("Error: По заданию нужно вводить через пробел");
+            throw new Exception("т.к. строка не является математической операцией");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } else if(data.length > 2)  try {
+            throw new Exception("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        //Определяет, находятся ли числа в одном формате (оба римские или оба арабские)
+        if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+            int result;
+            int a, b;
+            //Определяем, римские ли это числа
+            boolean isRoman = converter.isRoman(data[0]);
+            if (isRoman) {
+                //римские, то конвертируем их в арабские
+                a = converter.romanToInt(data[0]);
+                b = converter.romanToInt(data[1]);
+
+            } else {
+                //арабские, конвертирует в число
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
+            }
+
+            // ошибка
+            if(a > 10 || b > 10) {
+                try {
+                    throw new Exception("т.к. калькулятор принимает от 1 до 10");
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            //выполняет арифметическое действие через switch case
+            switch (actions[actionIndex]) {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                default:
+                    result = a / b;
+                    break;
+            }
+            // конвертирует в римские цифры
+            if (isRoman) {
+                // еслм римские, то конвертируем из арабских в римские и выводим ответ
+                if(result <= 0) {
+                    try {
+                        throw new Exception("т.к. в римской системе нет отрицательных чисел");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println(converter.intToRoman(result));
+                return "";
+            } else {
+                //арабские
+                System.out.println(result);
+                return "";
+            }
+        } else {
+            try {
+                throw new Exception("т.к. числа должны быть в одном формате");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
